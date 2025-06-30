cb566f869d5f95dbc459cdf73e8c95f6c12e35e0
Volker Bergen <31883718+VolkerBergen@users.noreply.github.com>
2025-06-30 21:58:19 +0200

    Google Colab compatibility (#11)

--- dilimap/__init__.py
+++ dilimap/__init__.py
@@ -1,8 +1,17 @@
 """DILImap - Predicting DILI risk using Toxicogenomics"""
 
+import os
+import sys
+import warnings
+
+# Set SCIPY_ARRAY_API for compatibility with Google colab
+if 'google.colab' in sys.modules and os.environ.get('SCIPY_ARRAY_API') != '1':
+    os.environ['SCIPY_ARRAY_API'] = '1'
+    if 'scipy' in sys.modules:
+        warnings.warn("'SCIPY_ARRAY_API' was set to '1' for compatibility in Colab.", stacklevel=2)
+
 from ._version import __version__  # hidden file
 from . import logging, s3, datasets, utils, models, clients, preprocessing as pp, plotting as pl
-import sys
 
 sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['pp', 'pl']})
 
